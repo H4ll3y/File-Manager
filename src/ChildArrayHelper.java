fb34ea0c3efa507c342e7aeca07fce133982df53
dmz <73778920+H4ll3y@users.noreply.github.com>
2022-06-21 17:18:22 +0700

    Add files via upload

---
+++ src/ChildArrayHelper.java
@@ -0,0 +1,9 @@
+import javafx.collections.ObservableList;
+
+public class ChildArrayHelper {
+    public static ObservableList<FileTreeItem> getChildren(FileTreeItem item)
+    {
+        if(item.getAbsolutePath().equals(Controller.hostName))return Controller.drives;
+        return item.childrenArray;
+    }
+}
